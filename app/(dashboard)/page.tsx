d689db36786e8dd6e9a09cde8053a7955d159776
leomiranda <leonardomir@gmail.com>
2025-01-02 17:24:30 -0400

    feat(api): get accounts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
+
 export default function Home() {
-	return <div>Overview</div>;
+	const { data: accounts, isLoading } = useGetAccounts();
+
+	if (isLoading) return <div>Loading...</div>;
+
+	return (
+		<div className="py-4">
+			<h1 className="text-2xl font-bold">Accounts</h1>
+			{accounts?.map((account) => (
+				<div key={account.id}>{account.name}</div>
+			))}
+		</div>
+	);
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,10 +1,19 @@
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 import { handle } from 'hono/vercel';
 import accounts from './accounts';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
+
+app.onError((err, c) => {
+	if (err instanceof HTTPException) {
+		return err.getResponse();
+	}
+
+	return c.json({ error: 'Internal Server Error' }, 500);
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route('/accounts', accounts);
