12f23d0745f36eb26f78c722a7a0a6251a9a5cd1
leomiranda <leonardomir@gmail.com>
2025-01-02 10:17:19 -0400

    feat: accounts router

---
+++ app/api/[[...route]]/accounts.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+	return c.json({
+		accounts: [],
+	});
+});
+
+export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,42 +1,12 @@
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import accounts from './accounts';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
-// app.use('*', clerkMiddleware());
-app
-	.get('/hello', clerkMiddleware(), (c) => {
-		const auth = getAuth(c);
-		if (!auth?.userId) {
-			return c.json({
-				message: 'You are not logged in.',
-			});
-		}
-		return c.json({
-			message: 'Hello Next.js!',
-			userId: auth.userId,
-		});
-	})
-	.get(
-		'/hello/:test',
-		zValidator(
-			'param',
-			z.object({
-				test: z.string(),
-			})
-		),
-		(c) => {
-			const { test } = c.req.valid('param');
-			return c.json({
-				message: 'Hello Next.js!',
-				test: test,
-			});
-		}
-	);
+
+app.route('/accounts', accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
