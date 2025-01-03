ba5128ee47c4b50a4aed8373ed1faf9a938eb4f5
leomiranda <leonardomir@gmail.com>
2025-01-03 15:51:55 -0400

    feat: account sheet

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
+import { useNewAccountStore } from '@/features/accounts/hooks/use-new-account';
 
 export default function Home() {
 	const { data: accounts, isLoading } = useGetAccounts();
+	const { onOpen } = useNewAccountStore();
 
 	if (isLoading) return <div>Loading...</div>;
 
 	return (
 		<div className="py-4">
 			<h1 className="text-2xl font-bold">Accounts</h1>
+			<Button onClick={onOpen}>New Account</Button>
 			{accounts?.map((account) => (
 				<div key={account.id}>{account.name}</div>
 			))}
